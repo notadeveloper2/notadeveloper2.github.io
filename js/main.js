699f9480ab227bd6cadf631c7c485b27cff9db50
Romain <105852474+notadeveloper2@users.noreply.github.com>
2023-02-14 18:32:16 +0100

    ca avance

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
+let lien1fromArticles = document.querySelector('.lien1');
+let lien2fromArticles = document.querySelector('.lien2');
+let lien3fromArticles = document.querySelector('.lien3');
 
-let lien = document.querySelectorAll('.lien');
+let lien1fromIndex = document.querySelector('.lien1index');
 
-function goToArticles() {
-    window.location.href = 'pages/articles.html';
+
+
+function goToArticle1FromIndex() {
+    window.location.href = 'pages/articles/article1.html';
 }
+function goToArticle1FromArticles() {
+    window.location.href = 'articles/article1.html';
+}
+function goToArticle2FromArticles() {
+    window.location.href = 'articles/article2.html';
+}
+function goToArticle3FromArticles() {
+    window.location.href = 'articles/article3.html';
+}
+
 function goToRadio() {
     window.location.href = 'pages/radio.html';
 }
 function goToVideo() {
     window.location.href = 'pages/vidoe.html';
 }
-lien[0].addEventListener("click", goToArticles, false) 
-lien[1].addEventListener("click", goToRadio, false) 
-lien[2].addEventListener("click", goToVideo, false) 
 
+lien1fromArticles.addEventListener("click", goToArticle1FromArticles, false)
+lien2fromArticles.addEventListener("click", goToArticle1FromArticles, false)
+lien3fromArticles.addEventListener("click", goToArticle1FromArticles, false)
+
+lien1fromIndex.addEventListener("click", goToArticle1FromIndex, false)
