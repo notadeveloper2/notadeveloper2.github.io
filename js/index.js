78a894c8614799ac317172b802b3c3de9c1f2eed
Romain <105852474+notadeveloper2@users.noreply.github.com>
2023-02-16 17:56:03 +0100

    radio fini

--- js/index.js
+++ js/index.js
@@ -5,3 +5,11 @@ function goToArticle1FromIndex() {
 }
 
 lien1fromIndex.addEventListener("click", goToArticle1FromIndex, false)
+
+let lien2fromIndex = document.querySelector('.lien2index');
+
+function goToArticle2FromIndex() {
+    window.location.href = 'pages/podcasts/podcast1.html';
+}
+
+lien2fromIndex.addEventListener("click", goToArticle2FromIndex, false)
