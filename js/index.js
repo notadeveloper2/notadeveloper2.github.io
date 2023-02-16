064d9d4774aeb578ec1b7b009dafee177510e32f
Romain <105852474+notadeveloper2@users.noreply.github.com>
2023-02-16 19:11:48 +0100

    video added

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ function goToArticle1FromIndex() {
 
 lien1fromIndex.addEventListener("click", goToArticle1FromIndex, false)
 
+
+
 let lien2fromIndex = document.querySelector('.lien2index');
 
 function goToArticle2FromIndex() {
@@ -13,3 +15,14 @@ function goToArticle2FromIndex() {
 }
 
 lien2fromIndex.addEventListener("click", goToArticle2FromIndex, false)
+
+
+
+
+let lien3fromIndex = document.querySelector('.lien3index');
+
+function goToArticle3FromIndex() {
+    window.location.href = 'pages/videos/video1.html';
+}
+
+lien3fromIndex.addEventListener("click", goToArticle3FromIndex, false)
