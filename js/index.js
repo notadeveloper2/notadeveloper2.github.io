7510342e8334a86b1d30abd2d0f501cc398dd80e
Romain <105852474+notadeveloper2@users.noreply.github.com>
2024-09-01 22:51:11 +0200

    site fini

--- js/index.js
+++ js/index.js
@@ -36,3 +36,63 @@ function goToArticle4FromIndex() {
 }
 
 lien4fromIndex.addEventListener("click", goToArticle4FromIndex, false)
+
+
+let lien5fromIndex = document.querySelector('.lien5index');
+
+function goToArticle5FromIndex() {
+    window.location.href = 'pages/articles/article5.html';
+}
+
+lien5fromIndex.addEventListener("click", goToArticle5FromIndex, false)
+
+
+
+let lien6fromIndex = document.querySelector('.lien6index');
+
+function goToArticle6FromIndex() {
+    window.location.href = 'pages/videos/video1.html';
+}
+
+lien6fromIndex.addEventListener("click", goToArticle6FromIndex, false)
+
+
+let lien7fromIndex = document.querySelector('.lien7index');
+
+function goToArticle7FromIndex() {
+    window.location.href = 'pages/videos/video2.html';
+}
+
+lien7fromIndex.addEventListener("click", goToArticle7FromIndex, false)
+
+
+let lien8fromIndex = document.querySelector('.lien8index');
+
+function goToArticle8FromIndex() {
+    window.location.href = 'pages/videos/video3.html';
+}
+
+lien8fromIndex.addEventListener("click", goToArticle8FromIndex, false)
+
+
+
+let lien9fromIndex = document.querySelector('.lien9index');
+
+function goToArticle9FromIndex() {
+    window.location.href = 'pages/podcasts/podcast1.html';
+}
+
+lien9fromIndex.addEventListener("click", goToArticle9FromIndex, false)
+
+
+
+
+
+
+let lien10fromIndex = document.querySelector('.lien10index');
+
+function goToArticle10FromIndex() {
+    window.location.href = 'pages/podcasts/podcast2.html';
+}
+
+lien10fromIndex.addEventListener("click", goToArticle10FromIndex, false)
