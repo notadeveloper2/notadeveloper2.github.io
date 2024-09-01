9cdf9ca9145b2f2144991d8a798215a63f871b02
Romain <105852474+notadeveloper2@users.noreply.github.com>
2024-09-01 19:29:04 +0200

    4 premiers articles

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ lien1fromIndex.addEventListener("click", goToArticle1FromIndex, false)
 let lien2fromIndex = document.querySelector('.lien2index');
 
 function goToArticle2FromIndex() {
-    window.location.href = 'pages/podcasts/podcast1.html';
+    window.location.href = 'pages/articles/article3.html';
 }
 
 lien2fromIndex.addEventListener("click", goToArticle2FromIndex, false)
@@ -22,7 +22,17 @@ lien2fromIndex.addEventListener("click", goToArticle2FromIndex, false)
 let lien3fromIndex = document.querySelector('.lien3index');
 
 function goToArticle3FromIndex() {
-    window.location.href = 'pages/videos/video1.html';
+    window.location.href = 'pages/articles/article2.html';
 }
 
 lien3fromIndex.addEventListener("click", goToArticle3FromIndex, false)
+
+
+
+let lien4fromIndex = document.querySelector('.lien4index');
+
+function goToArticle4FromIndex() {
+    window.location.href = 'pages/articles/article4.html';
+}
+
+lien4fromIndex.addEventListener("click", goToArticle4FromIndex, false)
